02f856377f1cbfd6e682507e6c1f7e3e0ee464b6
Jaimie Garner <pacificnm@gmail.com>
2026-06-10 16:40:04 -0700

    Fixed the Buildroot package metadata.

--- packages/foldingosctl/foldingosctl.mk
+++ packages/foldingosctl/foldingosctl.mk
@@ -7,6 +7,7 @@
 FOLDINGOSCTL_VERSION = 0.1.0
 FOLDINGOSCTL_SITE = $(TOPDIR)/../../../packages/foldingosctl/src
 FOLDINGOSCTL_SITE_METHOD = local
+FOLDINGOSCTL_GOMOD = foldingos.local/foldingosctl
 FOLDINGOSCTL_LICENSE = GPL-3.0-only
 FOLDINGOSCTL_LDFLAGS = -buildid=
 
